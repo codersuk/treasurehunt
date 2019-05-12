45e894e1fd23e9dcad1bc333818f1cb4224612f2
Bhavesh Ramburn <nitrogen@gmail.com>
2019-05-12 13:50:07 +0100

    page 1 completed

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -9,25 +9,37 @@ class PageSignupState extends State<PageSignup> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+      appBar: AppBar(title: Text('welcome'),)
+      ,
+      body: Center(
+        child: PageContainer(),
+      )
+    );
   }
 
   Widget PageContainer() {
     return Container(
-      padding: EdgeInsets.only(10),
+
       child: Column(
+
         children: <Widget>[
-          Text('Signup', style: TextStyle(
-            fontSize: 32,
-            color: Colors.black87,
+          Text(
+            'Signup',
+            style: TextStyle(
+              fontSize: 32,
+              color: Colors.black87,
+            ),
           ),
-
+          RaisedButton(
+            onPressed: null,
+            child: Text(
+              'Sign Up',
+              style: TextStyle(fontSize: 20),
+            ),
           )
-
         ],
-      )
-      ,
-
+      ),
     );
   }
 }
